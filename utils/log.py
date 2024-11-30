a826b6f456956230c3d4fd439991a6399718f07f
dezsocsete <dezso.csete@kadasolutions.ch>
2024-11-30 17:00:25 +0100

    s

--- utils/log.py
+++ utils/log.py
@@ -8,6 +8,6 @@ def setup_logging():
     log_format = "%(asctime)s [%(levelname)s] - %(message)s"
     logging.basicConfig(
         format=log_format, 
-        level=logging.debug,   # You can change this to INFO, WARNING, ERROR, etc.
+        level=logging.DEBUG,   # You can change this to INFO, WARNING, ERROR, etc.
         handlers=[logging.StreamHandler()]  # Output to console
     )
